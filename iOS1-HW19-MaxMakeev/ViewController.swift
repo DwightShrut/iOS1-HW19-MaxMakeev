27e56e4a8d57a3ee162ac991f1fa0c2704fe1139
DwightShrut <skatemax@mail.ru>
2025-08-13 18:46:23 +0300

    Initial Commit

---
+++ iOS1-HW19-MaxMakeev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS1-HW19-MaxMakeev
+//
+//  Created by Макс Макеев on 13.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
