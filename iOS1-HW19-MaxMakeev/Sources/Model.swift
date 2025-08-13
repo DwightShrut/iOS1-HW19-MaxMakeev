881327ac563e7e58ad973b3d6dbf43148380b736
DwightShrut <skatemax@mail.ru>
2025-08-14 01:04:37 +0300

    Update model

--- iOS1-HW19-MaxMakeev/Sources/Model.swift
+++ iOS1-HW19-MaxMakeev/Sources/Model.swift
@@ -11,16 +11,79 @@ import UIKit
 
 struct CellItem {
     let icon: UIImage?
+    let background: UIColor?
     let leftTitle: String?
-    let rightTitle: String?
+    var rightTitle: String?
     let isSwitch: Bool
     let switchValue: Bool
-    let action: UIAction?
 }
 
 enum CellType {
     case switchCell
     case defaultCell
     case titleCell
+}
+
+enum icons {
+    static let airplane = UIImage(systemName: "airplane")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 15, weight: .light))
+    static let wifi = UIImage(systemName: "wifi")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 15, weight: .light))
+    static let bluetooth = UIImage(systemName: "wave.3.forward")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 15, weight: .medium))
+    static let antenna = UIImage(systemName: "antenna.radiowaves.left.and.right")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 15, weight: .medium))
+    static let chain = UIImage(systemName: "personalhotspot")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 15, weight: .medium))
+    static let vpn = UIImage(systemName: "network")?.withConfiguration(UIImage.SymbolConfiguration(pointSize: 15, weight: .light))
+}
+
+
+
+let cellQueue: [CellType] = [
+    .switchCell,
+    .titleCell,
+    .titleCell,
+    .defaultCell,
+    .defaultCell,
+    .switchCell
+]
+
+extension CellItem {
+    static let settingsItems: [CellItem] = [
+        CellItem(
+            icon: icons.airplane, background: .systemOrange,
+            leftTitle: "Авиарежим",
+            isSwitch: true,
+            switchValue: true,
+        ),
+         CellItem(
+            icon: icons.wifi, background: .systemBlue,
+            leftTitle: "Wi-Fi",
+            rightTitle: "Выкл",
+            isSwitch: false,
+            switchValue: false,
+         ),
+         CellItem(
+            icon: icons.bluetooth, background: .systemBlue,
+            leftTitle: "Bluetooth",
+            rightTitle: "Вкл",
+            isSwitch: false,
+            switchValue: false,
+         ),
+         CellItem(
+            icon: icons.antenna, background: .systemGreen,
+            leftTitle: "Сотовая связь",
+            isSwitch: false,
+            switchValue: false
+         ),
+         CellItem(
+            icon: icons.chain, background: .systemGreen,
+            leftTitle: "Режим модема",
+            isSwitch: false,
+            switchValue: false
+         ),
+         CellItem(
+            icon: icons.vpn, background: .systemBlue,
+            leftTitle: "VPN",
+            isSwitch: false,
+            switchValue: false
+         )]
+    
 }
                     
