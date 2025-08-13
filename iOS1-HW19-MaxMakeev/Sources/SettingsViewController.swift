439c20d7e5d7cf130c2a4c2fd8fff4e701a2203f
DwightShrut <skatemax@mail.ru>
2025-08-13 20:03:31 +0300

    Create TableView in SettingsViewController

--- iOS1-HW19-MaxMakeev/Sources/SettingsViewController.swift
+++ iOS1-HW19-MaxMakeev/Sources/SettingsViewController.swift
@@ -8,10 +8,42 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        title = "Настройки"
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupLayout() {
+        tableView.addConstraints(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            topPadding: 0,
+            left: view.leadingAnchor,
+            leftPadding: 0,
+            right: view.trailingAnchor,
+            rightPadding: 0,
+            bottom: view.bottomAnchor,
+            bottomPadding: 0
+        )
     }
 
 
